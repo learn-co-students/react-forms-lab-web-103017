d949f981ec3fa4aaf223f87e7bb6b13843392ca5
Josh-Stillman <joshstillman@gmail.com>
2018-01-16 20:53:16 -0500

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,7 +4,35 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: ""
+    };
+  }
+
+  handleText = (event) => {
+    this.setState({poem: event.target.value})
+  }
+
+  isPoemValid = () => {
+    let lines = this.state.poem.split("\n")
+    if (lines.length !== 3) {
+      return false
+    }
+
+    let first = lines[0].trim().split(/\s+/)
+    let second = lines[1].trim().split(/\s+/)
+    let third = lines[2].trim().split(/\s+/)
+
+    if (first.length !== 5) {
+      return false
+    }
+    if (second.length !== 3) {
+      return false
+    }
+    if (third.length !== 5) {
+      return false
+    }
+    return true
   }
 
   render() {
@@ -13,13 +41,13 @@ class PoemWriter extends React.Component {
         <textarea
           rows="3"
           cols="60"
-        />
-        <div
+        value={this.state.poem} onChange={this.handleText}/>
+      {this.isPoemValid() ? <p>Valid Poem!</p> : <div
           id="poem-validation-error"
           style={{color: 'red'}}
         >
           This poem is not written in the right structure!
-        </div>
+        </div>}
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      textVal: ""
+    };
+  }
+
+  updateText = (event) => {
+    this.setState({textVal: event.target.value})
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.textVal} onChange={this.updateText}/>
+        <p> remaining characters is: {this.props.maxChars - this.state.textVal.length}</p>
       </div>
     );
   }
